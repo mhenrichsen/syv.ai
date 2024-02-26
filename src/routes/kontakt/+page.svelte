2894aeecdb56d51f34644b74723eb0323ac1a093
Mads Henrichsen <mads@BrbartiendeMads.lan>
2024-02-26 11:42:39 +0100

    add images and kontakt

---
+++ src/routes/kontakt/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	// You can add your script here
+</script>
+  
+<!--
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+-->
+
+<section class="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center">
+	<div class="p-8 md:p-12 lg:px-16 lg:py-24">
+	  <div class="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+		<h2 class="text-5xl font-bold text-gray-900 md:text-3xl">
+		  Lad os tage en snak
+		</h2>
+  
+		<p class="text-gray-500 md:mt-4">
+			Ring eller kontakt os via email, hvis du har spørgsmål. Vi sikre på, at vi kan finde frem til en løsning der passer perfekt til jer.
+		</p>
+
+		<p class="text-gray-500 md:mt-4">
+			Du kan fange os på telefon <a href="tel:27220804">27 22 08 04</a>, eller via email på <a href="mailto:mads@syv.ai">mads@syv.ai</a>.
+		</p>
+
+		<p class="text-gray-500 md:mt-4 md:block pt-10">
+			syv.ai <br>
+			Middelfartgade 18, 5. th <br>
+			2100 <br>
+			København Ø <br>
+			CVR: 34255334 <br>
+
+		</p>
+	  </div>
+	</div>
+  
+	<img
+	  alt=""
+	  src="/IMG_1635.png"
+	  class="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[120px]"
+	/>
+</section>
+  
+
+  
+<style>
+
+</style>
+  
